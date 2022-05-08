4f3c274e7d98836b5cf307233fe16a1b33e50255
Orozquiano <xorozcox5000@gmail.com>
2022-05-07 23:13:30 -0500

    Reorganizacion de archivos, navbar y tabs funcionando correctanmente @Autor: Juan Orozco

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,23 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'arma_tu_pc',
+        loadChildren: () => import('../armaTuPC_page/Arma_Tu_PC/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'informacion',
+        loadChildren: () => import('../informacion_page/Informacion/tab1.module').then(m => m.Tab1PageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/informacion',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/informacion',
     pathMatch: 'full'
   }
 ];
