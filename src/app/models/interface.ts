c7e602a7da0845cc8723e758319485cef076ee87
Orozquiano <xorozcox5000@gmail.com>
2022-05-13 00:24:08 -0500

    Se suber todas las interfaces que se van a usar para el desarrollo de Componapps @Autor: Juan Orozco

--- src/app/models/interface.ts
+++ src/app/models/interface.ts
@@ -32,5 +32,77 @@ export interface FPoder{
 export interface Grafica{
     Dx: number,
     Freq: number,
+    IMG: string,
+    Name: string,
+    OGL: number,
+    PCIe: number,
+    RayT: boolean,
+    Store: string,
+    TDP: number,
+    VRAM: number,
+    id: string
+}
 
+export interface Procesador{
+    Cache: number,
+    Cores: number,
+    Dx: number,
+    Freq: number,
+    FreqOC: number,
+    IGPU: boolean,
+    IMG: string,
+    Name: string,
+    OGL: number,
+    PCIe: number,
+    RamVel: number,
+    Socket: string,
+    Store: string,
+    Threads: number,
+    id: string
+}
+
+export interface RAM{
+    Capacity: number,
+    Freq: number,
+    IMG: string,
+    Name: string,
+    Quantity: number,
+    RamGen: string,
+    Store: string,
+    TDP: number,
+    id: string
+}
+
+export interface software{
+    CPUReq: {
+        Architectura: number,
+        Clock: number,
+        Cores: number,
+        Threads: number
+    },
+    Category: string,
+    GPUReq: {
+        Dx: number,
+        OGL: number,
+        VRAM: number
+    },
+    IMG: string,
+    Name: String,
+    RAM: number,
+    Storage: {
+        Capacity: number,
+        Format: string
+    },
+    id: string
+}
+
+export interface TMadre{
+    IMG: string,
+    Name: string,
+    PCIe: number,
+    RamGen: string,
+    Size: string,
+    Socket: string,
+    Store: string,
+    id: string   
 }
