e921faa59b9dc035784a53dc9d220eb7fcfc96ac
Orozquiano <xorozcox5000@gmail.com>
2022-05-13 22:44:20 -0500

    Se sube funcionalidad de quitar o poner software en la seccion de software seleccionado @Autor: Juan Orozco

--- src/app/armaTuPC_page/content_ArmaTuPc/explore-container.component.ts
+++ src/app/armaTuPC_page/content_ArmaTuPc/explore-container.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { element } from 'protractor';
 import { software } from 'src/app/models/interface';
 import { basedatosService } from 'src/app/service/basedatos.service';
 
@@ -15,6 +16,7 @@ export class ExploreContainerComponent implements OnInit {
   software: software[]=[];
   IMG: string;
   Name: string;
+  Info_Select_Soft: string[]=[];
 
   /** 
    * @constructor de la clase 
@@ -43,20 +45,38 @@ export class ExploreContainerComponent implements OnInit {
   /**
    * @function getInfo Trae la infor la base de datos (PRUEBA)
    */
+  
   getItems(){
     const enlace = 'Software';
     this.basedatosService.getInfo<software>(enlace).subscribe(res =>{
-      console.log(res);
-      this.software = res; //this.items.push(res[0]);
-      console.log(this.software[0].Name); 
+      this.software = res;
       this.IMG = this.software[0].IMG;
       this.Name = this.software[0].Name;
 
     });
   }
 
-  TomarIMG(){
-    console.log("HOLA")
+  /**
+   * @function Info_Software
+   * @param software Le paso toda la información de software
+   */
+
+  Info_Software(software){
+    if(this.Info_Select_Soft.length == 3){
+      alert("No puedes seleccionar mas de 3 software");
+    }else if(this.Info_Select_Soft.includes(software)){
+      alert("No puedes seleccionar el mismos software");
+
+    }else{
+      this.Info_Select_Soft.push(software);
+    }
+    
+
+  }
+
+  RemoveS(software_info){
+    let info = software_info.id;
+    this.Info_Select_Soft.splice(info,1);
   }
 
 }
